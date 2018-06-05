5834f722af8bb6759b9772b168984d2497856fb5
Jon Martin <jchristofmartin@gmail.com>
2018-06-04 21:54:44 -0500

    ui update

--- PixelScript/MainWindow.xaml.cs
+++ PixelScript/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace PixelScript {
     /// <summary>
@@ -19,7 +10,58 @@ namespace PixelScript {
     /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
-            InitializeComponent();
+            InitializeComponent();     
+        }
+
+        private BitmapSource img;
+        private void Select_OnClick(object sender, RoutedEventArgs e) {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Bitmap files (*.bmp)|*.bmp";
+            openFileDialog.Multiselect = false;
+            if (openFileDialog.ShowDialog() == false)
+                return;
+
+            img = new BitmapImage(new Uri(openFileDialog.FileNames[0]));
+            Image.Source = img;
+        }
+
+        private void Process_OnClick(object sender, RoutedEventArgs e) {
+            if (img == null)
+                return;
+
+            int stride = img.PixelWidth * 4;
+            int size = img.PixelHeight * stride;
+            byte[] pixels = new byte[size];
+            img.CopyPixels(pixels, stride, 0);
+
+            int xpos = 0;
+            int ypos = 0;
+
+            string[] colorAray = new string[pixels.Length / 4];
+            int colorIndex = 0;
+            for (int index = 0; index < pixels.Length; index += 4) {
+
+                byte blue = pixels[index];
+                byte green = pixels[index + 1];
+                byte red = pixels[index + 2];
+
+                byte alpha = pixels[index + 3];
+
+                int color = red << 16 | green << 8 | blue;
+                colorAray[colorIndex++] = color.ToString("X6");
+            }
+
+            for (int i = 0; i < colorAray.Length; i++) {
+                xpos = i % img.PixelWidth;
+                ypos = i / img.PixelWidth;
+
+                colorAray[i] = $@"<panel xpos=""{xpos * 10}"" ypos=""{ypos * 10}"" width=""10"" height=""10"" background=""#{colorAray[i]}""/>";
+                Console.WriteLine(colorAray[i]);
+            }
+        }
+
+        private void Exit_OnClick(object sender, RoutedEventArgs e) {
+            Application.Current.Shutdown();
         }
     }
 }
---
+++ PixelScript/ScriptOut.xaml.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Windows;
+
+
+namespace PixelScript {
+    /// <summary>
+    /// Interaction logic for ScriptOut.xaml
+    /// </summary>
+    public partial class ScriptOut : Window {
+        public ScriptOut() {
+            InitializeComponent();
+        }
+    }
+}
